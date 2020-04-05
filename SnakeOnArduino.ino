5fb210f80055420376f9567ece3537c4b7130de6
benlagreca02 <benlagreca02@gmail.com>
2020-04-04 22:07:34 -0400

    Added apple and it works now

--- SnakeOnArduino.ino
+++ SnakeOnArduino.ino
@@ -9,29 +9,39 @@ const char EMPTY_SPACE = '-';
 const char BORDER_PIECE = '#';
 const char HEAD_PIECE = 'S';
 const char SNAKE_BODY = 'X';
+const char APPLE_CHAR = 'A';
 
 //these are the Joystick Analog Pins
+const uint8_t OPEN_ANALOG_PIN = 5;
 const uint8_t XPIN = 0;
 const uint8_t YPIN = 1;
 const uint16_t UPPER_DEADBAND = 640; //512+128
 const uint16_t LOWER_DEADBAND = 384; //512-128
 
-//8 is up, 6 is right, 2 is down, 4 is left
+//this is how long every "frame" lasts
+const uint8_t REFRESH_RATE = 75;
+const uint8_t GROWTH_RATE = 3;
+
+//8 is up, 6 is right, 2 is down, 4 is left; these are just ambiguous numbers i picked
 const uint8_t   UP_CONST    = 8;
 const uint8_t   DOWN_CONST  = 2;
 const uint8_t   LEFT_CONST  = 4;
 const uint8_t   RIGHT_CONST = 6;
 
-
 //these are the starting positions for the snake head, which should be right in the middle of the grid
 uint8_t headX = GRID_MAX_X/2;
 uint8_t headY = GRID_MAX_Y/2;
 
+uint8_t appleX = 3;
+uint8_t appleY = 3;
+
+bool noApple = true;
+bool gameOver = false;
+uint16_t snakeLength = 3;
+
 
 //rememeber 0, 0 is the upper left corner
 char grid[GRID_MAX_Y][GRID_MAX_X];
-
-uint16_t snakeLength = 3;
 
 int tailX[((GRID_MAX_X)*(GRID_MAX_Y))]; //total area of the grid, can't get any bigger than that youd win or lose first
 int tailY[((GRID_MAX_X)*(GRID_MAX_Y))]; //should be GRID_MAX_X-2 and GRID_MAX_Y-2 but im making it bigger just incase
@@ -53,7 +63,7 @@ void ClearGrid()
     for(uint8_t y = 0; y < GRID_MAX_Y; y++)
     {
         grid[y][0] = BORDER_PIECE;
-        grid[y][GRID_MAX_X-1] = BORDER_PIECE;
+        grid[y][GRID_MAX_X-1] = BORDER_PIECE;   //minus one because the GRID_MAX_X const is the length, the coords are 0 to MAX-1
     }
     for(uint8_t x = 0; x < GRID_MAX_X; x++)
     {
@@ -145,13 +155,27 @@ void MoveSnake(uint8_t dir)
         tailY[i] = tailY[i-1];
     }
 
+    //check if we just ate an apple, look where the head is going to be
+    if(grid[headY][headX] == APPLE_CHAR)
+    {
+        snakeLength += GROWTH_RATE;
+        noApple = true;
+    }
+
+    if((grid[headY][headX] == BORDER_PIECE) or (grid[headY][headX] == SNAKE_BODY))
+    {
+        gameOver = true;
+    }
+
+
+
     //these set the snakes head to the 0th position after room has been made
     tailY[0] = headY;
     tailX[0] = headX;
 }
 
 
-void PrintSnake()
+void WriteSnake()
 {    
     grid[tailY[0]][tailX[0]] = HEAD_PIECE;
     
@@ -163,14 +187,35 @@ void PrintSnake()
 }
 
 
+//this function places a new apple on the grid by assigning to the variables which are then read in the PrintGrid function
+void WriteNewApple()
+{
+    bool pickingApple = true;
+    while(pickingApple)
+    {
+        uint8_t reqAppleX = random(1, GRID_MAX_X);
+        uint8_t reqAppleY = random(1, GRID_MAX_Y);
+        if(grid[reqAppleY][reqAppleX] == EMPTY_SPACE)
+        {
+            appleX = reqAppleX;
+            appleY = reqAppleY;
+            pickingApple = false;
+        }
+    }
+    noApple = false;
+
+}
+
+
 
 void setup()
 {
     Serial.begin(9600);
-    delay(100);
+    delay(REFRESH_RATE);
 
     ClearGrid();
     PrintGrid();
+    randomSeed(analogRead(OPEN_ANALOG_PIN)); //picks up noise and uses that noise for the seed for the randomization; this is what arduino website told me
 }
 
 
@@ -187,12 +232,19 @@ void loop()
         gameStarted = false;
     }
     
-    while(gameStarted)//yes its supposed to get stuck here its temp maybe
+
+    while(gameStarted && !gameOver)//yes its supposed to get stuck here its temp maybe
     {
-        MoveSnake(GetDir());
-        ClearGrid();
-        PrintSnake();
-        PrintGrid();
         delay(1000);
+        
+        ClearGrid();    //makes the grid blank
+        if(noApple)     //if theres no apple make one
+        {
+            WriteNewApple();
+        }    
+        grid[appleY][appleX] = APPLE_CHAR;
+        MoveSnake(GetDir());            //get the direction and move the snake
+        WriteSnake();                   //writes snake to grid
+        PrintGrid();                    //this is what does the serial outs and prints the grid with the snake and everything
     }
 }
